1a1ac16cd006860842e0df0ecc20315a0fb03993
Andrew CONAN <andrew.conan@laposte.fr>
2020-03-13 10:36:54 +0100

    creation class, repository, controller for user

---
+++ back/src/main/java/fr/mds/debazar/authentication/model/User.java
@@ -0,0 +1,36 @@
+package fr.mds.debazar.authentication.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+//@Table(name= "USERS")
+public class User {
+	
+	@Id
+	@GenericGenerator(name = "increment", strategy = "increment") 
+	@GeneratedValue(generator = "increment") 
+	private Long id;
+
+	private String username;
+	private String password;
+
+	private String mailAddress;
+	private String firstname;
+	private String lastname;
+	private String gender;
+	private String avatar;
+	private String color;
+	private Boolean verified;
+	private Boolean banned;
+
+}
