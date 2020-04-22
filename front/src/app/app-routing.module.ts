f48058d69f8ebf30cbdb6223b24e8d5fc20ee23b
Andrew CONAN <andrew.conan@laposte.fr>
2020-04-22 10:39:35 +0200

    Add AuthGuard to front with login system working

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -9,15 +9,19 @@ import { GameboxComponent } from "./pages/gamebox/gamebox.component";
 
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
+import { AuthGuard } from './global/auth/auth.guard';
 
 const routes: Routes = [
     { path: '', component: IndexComponent },
     { path: 'annonces', component: AdvertsComponent },
-    { path: 'vendre-mon-jeu', component: SellGameComponent },
+    
     { path: 'connexion', component: LoginComponent },
     { path: 'inscription', component: RegisterComponent },
-    { path: 'autour-de-moi', component: NearMeComponent },
-    { path: 'gamebox', component: GameboxComponent },
+
+    { path: 'vendre-mon-jeu', component: SellGameComponent, canActivate: [AuthGuard], },
+    { path: 'autour-de-moi', component: NearMeComponent, canActivate: [AuthGuard], },
+    { path: 'gamebox', component: GameboxComponent, canActivate: [AuthGuard], },
+
     { path: '**', component: IndexComponent }
 ];
 
@@ -25,7 +29,7 @@ const routes: Routes = [
     imports: [RouterModule.forRoot(
         routes,
         // Debug stuff
-        { enableTracing: true }
+        // { enableTracing: true }
         )],
     exports: [RouterModule]
 })
