3901559f4c7f604e5fc98dced250ed96aac28c3c
Alexis <alexis@agence-polux.fr>
2020-04-30 15:49:01 +0200

    - Added titles to index pages

--- front/src/app/global/components/index/popular-games/popular-games.component.ts
+++ front/src/app/global/components/index/popular-games/popular-games.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 import Game from 'src/app/global/models/Game';
 import Editor from 'src/app/global/models/Editor';
@@ -13,7 +14,8 @@ export class PopularGamesComponent implements OnInit {
 
     private games: Game[];
 
-    constructor() {
+    constructor(private titleService: Title) {
+        this.titleService.setTitle("Top Jeux - Débazar");
         this.games = [
             new Game(942, "Monopoly", "https://www.kroger.com/product/images/xlarge/front/0063050951263", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 4, "Éternité", new Date(), 3, 4, new Editor(1, "Bob"), new Category(1, "Jeu de plateau")),
             new Game(14887, "Uno", "https://images-na.ssl-images-amazon.com/images/I/810US3NaiqL._AC_SX425_.jpg", "Description", 8, "Éternité", new Date(), 2, 4, new Editor(2, "Jean-Luc Games"), new Category(2, "Jeu à deux")),
--- front/src/app/global/components/index/sell-games/sell-games.component.ts
+++ front/src/app/global/components/index/sell-games/sell-games.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-sell-games',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellGamesComponent implements OnInit {
 
-  constructor() { }
+    constructor(private titleService: Title) {
+        this.titleService.setTitle("Vendre son jeu - Débazar");
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+    }
 
 }
